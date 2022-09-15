0176fc3889c79f747ba45c37fe81867ab8086e4c
jerit6490 <jerit6490@2Q3H0Q2>
2022-09-15 13:40:59 -0400

    Add project files.

---
+++ Assign/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a;
+        int b;
+        Console.WriteLine("Assign Values:");
+        Console.WriteLine("\t a =" + (a = 8));
+        Console.WriteLine("\t b =" + (b = 4));
+        Console.WriteLine("\n\nAdd and Assign: ");
+        Console.WriteLine("\t a += b (8 += 4)\t a =" + (a += b));
+        Console.WriteLine("\n\nsubtract and Assign: ");
+        Console.WriteLine("\t a -= b (12 -= 4)\t a =" + (a -= b));
+        Console.WriteLine("\n\nMultiply and Assign:");
+        Console.WriteLine("\t a *= b (8 *=4) \t a=" + (a *= b));
+        Console.WriteLine("\n\nDivide and Assign: ");
+        Console.WriteLine("\t a /= b (32 /= 4)\t a=" + (a /= b));
+        Console.WriteLine("\n\nModulus and Assign: ");
+        Console.WriteLine("\t a %= b (8 %= 4)\t a=" + (a %= b));
+        Console.ReadKey();
+   
+    }
+}
